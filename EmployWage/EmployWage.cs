b546c036dbd9fee75d3180564508ee9be97ffef2
Pranav Waghmare <piyushw55@gmail.com>
2023-02-04 20:55:12 +0530

    check employee Present OR Not

---
+++ EmployWage/EmployWage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployWage
+{
+    internal class EmployWage
+    {
+        int empCheck;
+
+        public EmployWageUC1(int empChk)
+        {
+            this.empCheck = empChk;
+        }
+
+        public void Attendencs()
+        {
+            int IS_PRESENT = 1;
+            if (this.empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+
+
+    }
+}
---
+++ EmployWage/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployWage emp = new EmployWage();
+
+            emp.Attendencs();
+        }
+    }
+}
