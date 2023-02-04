5a460bc66fd33a221698a59fe8ccc8ade9ce51c1
Pranav Waghmare <piyushw55@gmail.com>
2023-02-04 23:18:41 +0530

    Calculate Wages till a condition of total Working Hours Of Days is reached for month

--- EmployWage/Program.cs
+++ EmployWage/Program.cs
@@ -5,28 +5,11 @@
         static void Main(string[] args)
         {
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = 0;
 
-            Console.WriteLine("\nEmployeeWage UC - 2");
-
-            empCheck = random.Next(0, 2);
-            EmployWageUC2 emp = new EmployWageUC2(empCheck);
-
-            emp.Attendences();
-
-            Console.WriteLine("\nEmployeeWage UC - 3");
             
-            empCheck = random.Next(0, 3);
-            EmployeeWageUC3 empuc3 = new EmployeeWageUC3(empCheck);
-            empuc3.Attendences();
 
-            Console.WriteLine("\nEmployeeWage UC - 4");
-
-            empCheck = random.Next(0, 3);
-            EmployeeWageUC4 empuc4 = new EmployeeWageUC4();
-            empuc4.Attendances(empCheck);
-
-            Console.WriteLine("\nEmployeeWage UC - 5");
+            Console.WriteLine("\nEmployeeWage UC - 6");
 
 
             empCheck = random.Next(0, 3);
